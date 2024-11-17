13ac9853008700952f8bf3355dfcb63a9a07ab97
MaryG88 <marinasoloviova80@gmail.com>
2024-11-17 15:32:55 +0100

    HW JS12 Finish Update

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -15,21 +15,21 @@ const gallery = document.querySelector('.gallery');
   });
 
 function renderImages(images) {
-//   
+  
   const markup = images
     .map(
       ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
        return `<li class="gallery-item">
-//     <a class="gallery-link" href="${largeImageURL}">
-//       <img class="gallery-img" src="${webformatURL}" alt="${tags}"  />
-//       <div class="gallery-img-info">
-//          <p class="info-item-title"><strong>Likes</strong>${likes}</p>
-//           <p class="info-item-title"><strong>Views</strong>${views}</p>
-//           <p class="info-item-title"><strong>Comments</strong>${comments}</p>
-//           <p class="info-item-title"><strong>Downloads</strong>${downloads}</p>
-//         </div>
-//     </a>
-//   </li>`;
+     <a class="gallery-link" href="${largeImageURL}">
+       <img class="gallery-img" src="${webformatURL}" alt="${tags}"  />
+       <div class="gallery-img-info">
+          <p class="info-item-title"><strong>Likes</strong>${likes}</p>
+          <p class="info-item-title"><strong>Views</strong>${views}</p>
+          <p class="info-item-title"><strong>Comments</strong>${comments}</p>
+          <p class="info-item-title"><strong>Downloads</strong>${downloads}</p>
+         </div>
+     </a>
+ </li>`;
       })
     .join('');
 
